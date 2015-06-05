e8d55c4d83b47eb1188468cc551cf6289a0d813d
TJ Mahr <mahr@wisc.edu>
2015-06-05 07:38:40 -0500

    checklist of libraries

---
+++ libraries.R
@@ -0,0 +1,26 @@
+# There are all the libraries are explicitly used in these scripts
+
+# Report generation
+library("knitr")
+library("rmarkdown")
+
+# Misc
+library("stringr")
+library("reshape2")
+library("gtools")
+
+# Data wrangling
+library("magrittr", warn.conflicts = FALSE)
+library("dplyr", warn.conflicts = FALSE)
+library("tidyr", warn.conflicts = FALSE)
+library("readr")
+
+# Graphics
+library("grid")
+library("ggplot2")
+
+# Models
+library("lme4")
+library("broom")
+library("AICcmodavg")
+
